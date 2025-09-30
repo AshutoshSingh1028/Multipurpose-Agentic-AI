f100af79f7bb82422b330272e8d0881e4b98d4e8
AshutoshSingh1028 <ashutosh.singh1028@gmail.com>
2025-10-01 02:57:52 +0530

    Added tools

--- src/langgraphagenticai/tools/search_tool.py
+++ src/langgraphagenticai/tools/search_tool.py
@@ -1,15 +1,19 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import ToolNode
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.utilities.arxiv import ArxivAPIWrapper
+from langchain_community.tools.arxiv.tool import ArxivQueryRun
 
 def get_tools():
     """
     Return the list of tools to be used in the chatbot.
     """
-    tools = [TavilySearchResults(max_results=2)]
+    tools = [TavilySearchResults(max_results=2), WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper()), ArxivQueryRun(api_wrapper=ArxivAPIWrapper())]
     return tools
 
 def create_tool_node(tools):
     """
     Creates and returns a tool node for the graph
     """
-    return ToolNode(tools=tools)
+    return ToolNode(tools=tools) 
